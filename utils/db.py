8dc9a0578d0fc46dc87943ac2050cd7e28133d89
Xanooo <tamineha@yahoo.fr>
2025-10-28 18:07:11 +0100

    feat: Connexion à la BD et création de la table Annonces.

--- utils/db.py
+++ utils/db.py
@@ -1,18 +1,34 @@
-import psycopg2, os 
+import psycopg2, os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 def get_connection():
-    return psycopg2.connect(os.getenv("DB_CONFIG"))
+    """
+    Établit une connexion à la base de données PostgreSQL en utilisant les variables d'environnement.
+    """
+    return psycopg2.connect(
+        dbname=os.getenv("PG_DB"),
+        user=os.getenv("PG_USER"),
+        password=os.getenv("PG_PASSWORD"),
+        host=os.getenv("PG_HOST", "localhost"),
+        port=os.getenv("PG_PORT", "5432"),
+    )
 
 def create_tables():
+    """
+    Crée les tables nécessaires dans la base de données si elles n'existent pas déjà.
+    """
     connexion = get_connection()
     cursor = connexion.cursor()
 
-    connexion.execute("""CREATE TABLE IF NOT EXISTS annonces (
-            id INT PRIMARY KEY,
-            name VARCHAR(255),
-            price INT(50),
-            surface INT(50),
-            url VARCHAR(255),
+    cursor.execute("""CREATE TABLE IF NOT EXISTS annonces (
+            id SERIAL PRIMARY KEY,
+            name TEXT,
+            price INT,
+            surface INT,
+            url TEXT);
         """)
 
     connexion.commit()
